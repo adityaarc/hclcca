7d0301fab00061c6cc7c8f664e4b474375a79cb3
ADITYA RAJ CHOUDHARY <55029118+adityaarc@users.noreply.github.com>
2021-08-25 19:44:16 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,82 @@
+// creating an array and passing the number, questions, options, and answers
+
+/*
+
+What country does Colin Munro come from?
+
+o England
+o Australia
+o New Zealand
+o South Africa
+*/
+let questions = [
+    {
+    numb: 1,
+    question: "In which venues, the BBL matches was conducted in Australia?",
+    answer: "Victoria and New South Wales",
+    options: [
+      "Western Australia and Northern Territory",
+      "Victoria and New South Wales",
+      "Queensland and Northern Territory",
+      "Adelaide and Northern Territory"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who is the current captain of Melbourne Renegades?",
+    answer: "Aaron Finch",
+    options: [
+      "Joe Root",
+      "Glenn Maxwell",
+      "Aaron Finch",
+      "Rohit Sharma"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Glenn Maxwell has played for the Melbourne Renegades. What other BBL team has he played for?",
+    answer: "Melbourne Stars",
+    options: [
+      "Melbourne Stars",
+      "Brisbane Heat",
+      "Adelaide Strikers",
+      "Perth Scorchers"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the main colour of the Perth Scorchers?",
+    answer: "Orange",
+    options: [
+      "Purple",
+      "Orange",
+      "Pink",
+      "Green"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What country does Colin Munro come from?",
+    answer: "New Zealand",
+    options: [
+      "New Zealand",
+      "South Africa",
+      "Australia",
+      "England"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
